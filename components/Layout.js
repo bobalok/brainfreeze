5e27078f461c7ba4b8d93aa34b5f63b20e579d85
technocalorie <findabirr@gmail.com>
2021-12-27 17:31:27 +0600

    fix bg img

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,22 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Navigation from "./Navigation";
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      {/* <Navigation /> */}
-      <main className="bg-purple-900">{children}</main>
+      <main className="bg-tatsuBG bg-hero-pattern bg-hero-size">
+        {/* <Image
+          className=""
+          src="/images/landing-page-background-pattern.png"
+          alt="Picture of the author"
+          layout="intrinsic"
+          width={2881}
+          height={820}
+        /> */}
+        {children}
+      </main>
+
       <Footer />
     </>
   );
