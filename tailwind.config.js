1d540bc78363887126c7448a48c0d7457823793d
technocalorie <findabirr@gmail.com>
2021-12-27 17:30:28 +0600

    background image added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      colors: {
+        tatsuBG: '#282a2e',
+        tatsuBTN: '#5865f2',
+        tatsuBTN2: '#00a3ff',
+      },
       backgroundImage: {
-        "hero-pattern": "url('/assets/img/background.svg')",
+        'hero-pattern': "url('/images/landing-page-background-pattern.png')",
+      },
+      backgroundSize: {
+        'hero-size': '1100px',
       },
     },
   },
