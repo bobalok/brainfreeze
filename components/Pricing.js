059778aff53bc4a80b6f1e0940f43a5be304cb48
technocalorie <findabirr@gmail.com>
2022-01-02 18:03:13 +0600

    Price Component added

---
+++ components/Pricing.js
@@ -0,0 +1,163 @@
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/solid";
+
+export default function Pricing() {
+  return (
+    <>
+      <section className="text-white">
+        <div className="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8 grid md:grid-cols-4 gap-2">
+          <div className="flex flex-col md:flex-row px-2 md:px-0">
+            <div className="w-full md:w-1/1 bg-white rounded-lg shadow hover:shadow-xl transition duration-100 ease-in-out p-6 md:mr-4 mb-10 md:mb-0">
+              <h3 className="text-gray-600 text-lg">Basic</h3>
+              <p className="text-gray-600 mt-1">
+                <span className="font-bold text-black text-4xl">৳700</span>{" "}
+                /Month
+              </p>
+              <p className="text-sm text-gray-600 mt-2">
+                For most businesses that want to optimize web queries.
+              </p>
+              <div className="text-sm text-gray-600 mt-4 ">
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+              </div>
+              <button className="w-full text-purple-700 border border-purple-700 rounded hover:bg-purple-700 hover:text-white hover:shadow-xl transition duration-150 ease-in-out py-4 mt-4">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row px-2 md:px-0">
+            <div className="w-full md:w-1/1 bg-white rounded-lg shadow hover:shadow-xl transition duration-100 ease-in-out p-6 md:mr-4 mb-10 md:mb-0">
+              <h3 className="text-gray-600 text-lg">Base</h3>
+              <p className="text-gray-600 mt-1">
+                <span className="font-bold text-black text-4xl">৳69</span>{" "}
+                /Month
+              </p>
+              <p className="text-sm text-gray-600 mt-2">
+                For most businesses that want to optimize web queries.
+              </p>
+              <div className="text-sm text-gray-600 mt-4 ">
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+              </div>
+              <button className="w-full text-purple-700 border border-purple-700 rounded hover:bg-purple-700 hover:text-white hover:shadow-xl transition duration-150 ease-in-out py-4 mt-4">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row px-2 md:px-0">
+            <div className="w-full md:w-1/1 bg-white rounded-lg shadow hover:shadow-xl transition duration-100 ease-in-out p-6 md:mr-4 mb-10 md:mb-0">
+              <h3 className="text-gray-600 text-lg">Base</h3>
+              <p className="text-gray-600 mt-1">
+                <span className="font-bold text-black text-4xl">৳69</span>{" "}
+                /Month
+              </p>
+              <p className="text-sm text-gray-600 mt-2">
+                For most businesses that want to optimize web queries.
+              </p>
+              <div className="text-sm text-gray-600 mt-4 ">
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+              </div>
+              <button className="w-full text-purple-700 border border-purple-700 rounded hover:bg-purple-700 hover:text-white hover:shadow-xl transition duration-150 ease-in-out py-4 mt-4">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row px-2 md:px-0">
+            <div className="w-full md:w-1/1 bg-white rounded-lg shadow hover:shadow-xl transition duration-100 ease-in-out p-6 md:mr-4 mb-10 md:mb-0">
+              <h3 className="text-gray-600 text-lg">Base</h3>
+              <p className="text-gray-600 mt-1">
+                <span className="font-bold text-black text-4xl">৳1000</span>{" "}
+                /Month
+              </p>
+              <p className="text-sm text-gray-600 mt-2">
+                For most businesses that want to optimize web queries.
+              </p>
+              <div className="text-sm text-gray-600 mt-4 ">
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+                <p className="my-2 flex">
+                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  <span>All limited links</span>
+                </p>
+              </div>
+              <button className="w-full text-purple-700 border border-purple-700 rounded hover:bg-purple-700 hover:text-white hover:shadow-xl transition duration-150 ease-in-out py-4 mt-4">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
