0c0482bbf131e353c325b8d9d0cf95f6344978f9
technocalorie <findabirr@gmail.com>
2022-01-12 15:17:08 +0600

    disabled testis

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -141,7 +141,7 @@ export default function Home() {
         <div className="max-w-screen-xl w-full h-[2.5px] bg-gradient-to-l from-gray-700 via-gray-100 to-gray-700"></div>
         {/* ############################################################################################### */}
 
-        <Testimonials />
+        {/* <Testimonials /> */}
 
         {/* ############################################################################################### */}
         <div className="max-w-screen-xl w-full h-[1.5px] bg-gradient-to-l from-gray-700 via-gray-100 to-gray-700"></div>
