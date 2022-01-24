259f2f35f4cb600eab16f15f3b31956811e897e0
technocalorie <findabirr@gmail.com>
2022-01-24 19:06:49 +0600

    fake active added need to fix later

--- components/Navi.js
+++ components/Navi.js
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
   BriefcaseIcon,
@@ -10,13 +10,13 @@ import {
 import Link from "next/link";
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
+  { name: "Home", href: "/", current: false },
   { name: "Gaming", href: "/dashboard", current: false },
-  { name: "Pricing", href: "#", current: false },
+  { name: "Pricing", href: "/pricing", current: false },
   { name: "Blog", href: "#", current: false },
   { name: "Contact", href: "#", current: false },
 ];
-
+false;
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -33,50 +33,50 @@ function MyLink(props) {
 export default function Navi() {
   return (
     <Disclosure
-      as="nav"
-      className="stiky top-0 z-50 w-full backdrop-blur-0 bg-black/90"
+      as='nav'
+      className='stiky top-0 z-50 w-full backdrop-blur-0 bg-black/90'
     >
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-24">
-              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex items-center justify-between h-24'>
+              <div className='absolute inset-y-0 left-0 flex items-center md:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                  <span className='sr-only'>Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XIcon className='block h-6 w-6' aria-hidden='true' />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
-                <div className="flex-shrink-0 flex items-center">
+              <div className='flex-1 flex items-center justify-center md:items-stretch md:justify-start'>
+                <div className='flex-shrink-0 flex items-center'>
                   <img
-                    className="block md:hidden h-14 w-auto"
-                    src="/neutechLogobwNoText.svg"
+                    className='block md:hidden h-14 w-auto'
+                    src='/neutechLogobwNoText.svg'
                     // src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="neutech logo"
+                    alt='neutech logo'
                   />
                   <img
-                    className="hidden md:block h-14 w-auto"
-                    src="/neutechLogobwWithText.svg"
+                    className='hidden md:block h-14 w-auto'
+                    src='/neutechLogobwWithText.svg'
                     // src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="neutech logo"
+                    alt='neutech logo'
                   />
                 </div>
-                <div className="hidden md:block sm:ml-6">
-                  <div className="flex md:space-x-0 lg:space-x-4 py-4">
+                <div className='hidden md:block sm:ml-6'>
+                  <div className='flex md:space-x-0 lg:space-x-4 py-4'>
                     {navigation.map((item) => (
                       <MyLink
                         key={item.name}
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "md:text-tatsuBTN2 xs:bg-gray-900"
-                            : "text-gray-300 xs:bg-gray-700 hover:text-tatsuBTN",
-                          "px-3 py-2 rounded-md text-md font-medium"
+                            ? "focus:text-tatsuBTN2 xs:bg-gray-900"
+                            : "text-gray-300 hover:text-tatsuBTN",
+                          "px-3 py-2 rounded-md text-md font-medium focus:text-tatsuBTN2"
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
@@ -86,9 +86,9 @@ export default function Navi() {
                   </div>
                 </div>
               </div>
-              <div className=" absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button className="hidden bg-tatsuBTN hover:bg-white transition-all ease-linear hover:text-tatsuBTN py-4 px-4 border-2 border-indigo-400 hover:border-transparent rounded md:inline-flex items-center text-white text-base font-bold">
-                  <BriefcaseIcon className="h-6 w-6 mr-1" aria-hidden="true" />
+              <div className=' absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                <button className='hidden bg-tatsuBTN hover:bg-white transition-all ease-linear hover:text-tatsuBTN py-4 px-4 border-2 border-indigo-400 hover:border-transparent rounded md:inline-flex items-center text-white text-base font-bold'>
+                  <BriefcaseIcon className='h-6 w-6 mr-1' aria-hidden='true' />
                   <span>We are hiring!</span>
                 </button>
 
@@ -168,12 +168,12 @@ export default function Navi() {
             </div>
           </div>
 
-          <Disclosure.Panel className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+          <Disclosure.Panel className='md:hidden'>
+            <div className='px-2 pt-2 pb-3 space-y-1'>
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
-                  as="a"
+                  as='a'
                   href={item.href}
                   className={classNames(
                     item.current
