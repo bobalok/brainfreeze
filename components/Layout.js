9abfb68edc5a81b266d9c160a914191e692c0d6a
technocalorie <findabirr@gmail.com>
2022-02-06 02:01:42 +0600

    clean Layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,23 +2,19 @@
 
 import Navi from "./Navi";
 import Footer from "./Footer";
-import ContactUs from "./ContactUs";
-import HorizontalLine from "./HorizontalLine";
 import ScrollToTop from "react-scroll-to-top";
 import { ChevronDoubleUpIcon } from "@heroicons/react/solid";
-import react from "react";
-
 export default function Layout({ children }) {
   return (
     <>
       <Navi />
       <ScrollToTop
-        color="red"
+        color='red'
         smooth
         top={1000}
         component={<ChevronDoubleUpIcon />}
       />
-      <main className="bg-tatsuBG bg-hero-pattern bg-hero-size">
+      <main className='bg-tatsuBG bg-hero-pattern bg-hero-size'>
         {/* <Image
           className=""
           src="/images/landing-page-background-pattern.png"
