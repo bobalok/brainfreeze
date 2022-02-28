3fb16ff62f15fc1b84407bcb4041c0633330675d
technocalorie <findabirr@gmail.com>
2022-02-28 18:26:54 +0600

    High Availibility >> Achieve more with betternet

--- components/HANetwork.js
+++ components/HANetwork.js
@@ -7,11 +7,15 @@ export default function HANetwork() {
         <div className='xl:max-w-5xl 2xl:max-w-7xl mx-auto px-4 md:py-16 sm:px-6 lg:px-8'>
           <div className=''>
             <h1 className='text-3xl font-bold sm:text-5xl  pb-4 md:text-center mt-6'>
-              High Availability 24x7
+              Achieve more with{" "}
+              <span className='text-3xl font-extrabold sm:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-sky-400 to-blue-500'>
+                betternet
+              </span>
             </h1>
             <p className='0 md:text-center max-w-xl mx-auto'>
               Your Internet should be at its best every day. We’re committed to
               investing in the capacity and reliability of our network
+              infrastructure, <strong>better experience assured.</strong>
             </p>
           </div>
           <video
