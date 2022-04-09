25a71500643cb75213051277c7e925c9f30f21b4
technocalorie <findabirr@gmail.com>
2022-04-09 16:22:45 +0600

    IoT to Advance Network Filtration

--- components/WhatMakesUsDifferent.js
+++ components/WhatMakesUsDifferent.js
@@ -109,14 +109,14 @@ export default function WhatMakesUsDifferent() {
                     strokeLinecap='round'
                     strokeLinejoin='round'
                     strokeWidth={2}
-                    d='M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z'
+                    d='M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z'
                   />
                 </svg>
               </span>
 
               <div className='ml-3 md:mt-3 md:ml-0'>
                 <dt className='text-xl dark:text-white text-textGray font-medium'>
-                  Smart IoT Implementation
+                  Advance Network Filtration
                 </dt>
 
                 <dd className='mt-1 dark:text-white text-textGray'>
@@ -190,7 +190,7 @@ export default function WhatMakesUsDifferent() {
 
               <div className='ml-3 md:mt-3 md:ml-0'>
                 <dt className='text-xl font-medium dark:text-white text-textGray'>
-                  Work Station Optimization
+                  Workstation Optimization
                 </dt>
 
                 <dd className='mt-1 dark:text-white text-textGray'>
