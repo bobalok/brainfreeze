0efed434da1f92c90478731a82ead7074113f69d
technocalorie <findabirr@gmail.com>
2022-11-20 13:33:01 +0600

    updated with latest packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-// module.exports = {
-//   // Use the CDN in production and localhost for development.
-//     basePath: '/brainfreeze',
-//     assetPrefix: 'https://technocalorie.github.io/brainfreeze',
-// };
-
+/** @type {import('next').NextConfig} */
 module.exports = {
+  reactStrictMode: true,
   images: {
     domains: [
       "https://drive.google.com",
